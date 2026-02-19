a5f07867cf99791420f52c1ca27fd341c57d2ef6
Estevão Locks <160160810+EstevaoLocks@users.noreply.github.com>
2026-02-19 09:23:58 -0300

    add aula02

---
+++ aulas/aula02.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nota = 7;
+    if ($nota >= 6){
+        echo "Aprovado!";
+    } else {
+        echo "Reprovado!";
+    }
+    echo "<br>";
+    for ($i = 1; $i <= 5; $i++){
+        echo "Número: " . $i . "<br>";
+    }
+
+
+?>
