0b87d9b74f6a6879bf4820972a392de6dad0ea74
estevaolocks <estevao.locks11@gmail.com>
2026-02-26 10:23:56 -0300

    salvando exercicios 2 e 3

---
+++ aulas/aula02/exercicios/exercicio02.php
@@ -0,0 +1,14 @@
+<?php
+
+$number = 173;
+echo "Número: " . $number . "<hr>";
+
+if ($number > 0){
+    echo "Este número é positivo";
+}elseif ($number < 0){
+    echo "Este é um número negativo";
+}else {
+    echo "Este número é zero";
+}
+
+?>
---
+++ aulas/aula02/exercicios/exercicio03.php
@@ -0,0 +1,12 @@
+<?php
+
+$number = 5;
+echo "Número: " . $number . "<hr>";
+
+if ($number % 2 == 0){
+    echo "Este número é par";
+}else {
+    echo "Este número é impar";
+}
+
+?>
